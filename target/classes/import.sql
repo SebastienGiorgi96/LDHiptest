17d42c0b60ea9b2c8f40bcba2406208b2a621bc8
CNAMTS\GIORGI <sebastien.giorgi@assurance-maladie.fr>
2018-11-13 11:37:07 +0100

    commit test

---
+++ target/classes/import.sql
@@ -0,0 +1,17 @@
+INSERT INTO team(name) VALUES('Real Madrid');
+INSERT INTO team(name) VALUES('Juventus Turin');
+INSERT INTO team(name) VALUES('FC Barcelone');
+INSERT INTO team(name) VALUES('Bayern Munich');
+INSERT INTO team(name) VALUES('SSC Napoli');
+INSERT INTO team(name) VALUES('Liverpool FC');
+INSERT INTO team(name) VALUES('Chelsea FC');
+INSERT INTO team(name) VALUES('Atletico Madrid');
+
+INSERT INTO player(nom, num, poste, team_id) VALUES('Ronaldo',7,'Attaquant',2);
+INSERT INTO player(nom, num, poste, team_id) VALUES('Dybala',21,'Attaquant',2);
+INSERT INTO player(nom, num, poste, team_id) VALUES('Sergio Ramos',4,'Defenseur',1);
+INSERT INTO player(nom, num, poste, team_id) VALUES('Varane',2,'Defenseur',1);
+INSERT INTO player(nom, num, poste, team_id) VALUES('Modric',10,'Milieu',1);
+INSERT INTO player(nom, num, poste, team_id) VALUES('Kroos',8,'Milieu',1);
+INSERT INTO player(nom, num, poste, team_id) VALUES('Salah',11,'Attaquant',6);
+INSERT INTO player(nom, num, poste, team_id) VALUES('Mané',10,'Attaquant',6);
